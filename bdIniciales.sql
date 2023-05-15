37ff6d6505eda9ce1e7301345f56753e09aaff89
JARED <109309699+ro047825@users.noreply.github.com>
2023-05-15 17:55:46 -0600

    Add files via upload

---
+++ bdIniciales.sql
@@ -0,0 +1,15 @@
+create database RJO
+
+use RJO
+
+create table Sucursal(
+IdSucursal varchar(10)not null,
+No_Comercial varchar(30)not null,
+Telefono varchar(30)not null,
+Email varchar(30)not null,
+Suc_Matriz char(20)not null,
+Nombre varchar(20)not null,
+Fecha date,
+No_Modificado varchar(20)not null,
+Fecha_Modifacada date,
+);
